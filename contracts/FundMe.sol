4ce8fcc081c0a930d88e68d0032a07e72a76e6d6
Sayed Zahed <rumiocean@tutanota.com>
2026-05-20 22:14:57 +0900

    FundMe v2 - Chainlink + CEI

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.34;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from
+"@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+contract FundMe {
 
-contract FundMe{
     address public owner;
     mapping(address => uint256) public addressToAmount;
     address[] public funders;
@@ -14,39 +15,50 @@ contract FundMe{
         owner = msg.sender;
     }
 
-    function fund() public payable{
-        require(getConversionRate(msg.value) >= MINIMUM_USD, "Didn't send enough ETH");
-
+    function fund() public payable {
+        require(
+            getConversionRate(msg.value) >= MINIMUM_USD,
+            "Didn't send enough ETH"
+        );
         addressToAmount[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
 
-    function withdraw() public{
+    function withdraw() public {
         require(msg.sender == owner, "Not the owner");
+
+        for(uint256 i = 0; i < funders.length; i++) {
+             addressToAmount[funders[i]] = 0;
+
+        }
+        funders = new address[](0);
+
         payable(owner).transfer(address(this).balance);
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return address(this).balance;
     }
 
-    function getFunderAddress(uint256 _index) public view returns(address){
+    function getFunderAddress(uint256 _index)
+        public view returns(address) {
         return funders[_index];
     }
 
-    function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed =
+            AggregatorV3Interface(
+                0x694AA1769357215DE4FAC081bf1f309aDC325306
+            );
         (, int256 price,,,) = priceFeed.latestRoundData();
         return uint256(price * 1e10);
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
+    function getConversionRate(uint256 ethAmount)
+        public view returns(uint256) {
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        uint256 ethAmountInUsd =
+            (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
     }
-
-
 }
